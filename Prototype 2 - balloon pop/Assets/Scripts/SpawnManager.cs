3a55e11781b1d2fa55577e13f92062e94615416f
Zaelet <dylan.p611@stu.nebo.edu>
2025-09-25 08:49:15 -0600

    reduced balloon collisions with each other by slightly offsetting zPosition

--- Prototype 2 - balloon pop/Assets/Scripts/SpawnManager.cs
+++ Prototype 2 - balloon pop/Assets/Scripts/SpawnManager.cs
@@ -8,6 +8,7 @@ public class SpawnManager : MonoBehaviour
     public float startDelay = 0.5f; // time before first balloon
     public float spawnInterval = 1.5f; // time between balloon spawns
     public float xRange = 10.0f;
+    public float zRange = 5.0f;
     public float yPos = -10.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,7 +19,7 @@ public class SpawnManager : MonoBehaviour
     void SpawnRandomBalloon()
     {
         //get random position on the x axis
-        Vector3 spawnPos = new Vector3(Random.Range(-xRange, xRange), yPos, 0);
+        Vector3 spawnPos = new Vector3(Random.Range(-xRange, xRange), yPos, Random.Range(-zRange, zRange));
         // pick a random balloon from our array of prefabs
         int balloonIndex = Random.Range(0, balloonPrefabs.Length);
         // spawn the random balloon at the random position we chose
