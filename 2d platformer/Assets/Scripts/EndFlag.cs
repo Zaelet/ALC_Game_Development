9a19e4df3d86fbdf5923ad8ef2d0836a378b081a
Zaelet <dylan.p611@stu.nebo.edu>
2025-11-03 09:09:42 -0700

    attempted to use animator to make door only open when touched, failed, few changes

--- 2d platformer/Assets/Scripts/EndFlag.cs
+++ 2d platformer/Assets/Scripts/EndFlag.cs
@@ -7,10 +7,25 @@ public class EndFlag : MonoBehaviour
 {
     public bool finalLevel;
     public string nextLevelName;
+    public Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+         animator.SetBool("OpenDoor", false);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-      
+
+        if (collision.CompareTag("Player"))
+        {
+            animator.SetBool("OpenDoor", true);
+        }
+        else
+        {
+            animator.SetBool("OpenDoor", false);
+        }
+        
         if (collision.CompareTag("Player"))
         {
             if (finalLevel == true)
