d232d93b83f1905d2439bf1b55b6355a25f7ca01
Zaelet <dylan.p611@stu.nebo.edu>
2025-11-07 09:19:12 -0700

    2D prototype passed off

--- 2d platformer/Assets/Scripts/EndFlag.cs
+++ 2d platformer/Assets/Scripts/EndFlag.cs
@@ -11,22 +11,24 @@ public class EndFlag : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
-         animator.SetBool("OpenDoor", false);
+        animator.SetBool("OpenDoor", false);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player")) //if the player touched me continue
         {
+            
+            //check if current level is final level, go to next level/menu
             if (finalLevel == true)
             {
-                animator.SetBool("OpenDoor", true);
-                //if( this.animator.GetCurrentAnimatorStateInfo(0).IsName("OpenDoor") )
+                animator.SetBool("OpenDoor", true); //open door is now true
+                //if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("OpenDoor")) if the animator is currently playing OpenDoor load the scene
                 //{
-                   SceneManager.LoadScene(0); 
-               // }
-                
+                    SceneManager.LoadScene(0);
+                //}
+                //set open door to false and rerun?
             }
             else
             {
